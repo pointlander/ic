00fdb89914bc2865734d731d5cae9d496bf6d6ff
Andrew Snodgrass <pointlander@gmail.com>
2024-05-11 10:14:25 -0500

    Added books

--- books/bible.go
+++ books/bible.go
@@ -6,26 +6,40 @@ package books
 
 import (
 	"bytes"
-	"compress/gzip"
+	"compress/bzip2"
 	"embed"
 	"io"
 )
 
-//go:embed 10.txt.utf-8.gz
+//go:embed *.bz2
 var f embed.FS
 
 func GetBible() []byte {
-	data, err := f.ReadFile("10.txt.utf-8.gz")
-	if err != nil {
-		panic(err)
+	bible := []byte{}
+	files := []string{
+		"10.txt.utf-8.bz2",
+		"130.txt.utf-8.bz2",
+		"131.txt.utf-8.bz2",
+		"398.txt.utf-8.bz2",
+		"470.txt.utf-8.bz2",
+		"1653.txt.utf-8.bz2",
+		"3296.txt.utf-8.bz2",
+		"36402.txt.utf-8.bz2",
+		"5657.txt.utf-8.bz2",
+		"57121.txt.utf-8.bz2",
+		"59041.txt.utf-8.bz2",
 	}
-	reader, err := gzip.NewReader(bytes.NewReader(data))
-	if err != nil {
-		panic(err)
+	for _, file := range files {
+		data, err := f.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+		reader := bzip2.NewReader(bytes.NewReader(data))
+		data, err = io.ReadAll(reader)
+		if err != nil {
+			panic(err)
+		}
+		bible = append(bible, data...)
 	}
-	data, err = io.ReadAll(reader)
-	if err != nil {
-		panic(err)
-	}
-	return data
+	return bible
 }
